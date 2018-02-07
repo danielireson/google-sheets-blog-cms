1528ac795a643d602efa71fad74b7d0191e4a2ce
Daniel Ireson <daniel.ireson@zestia.com>
2018-02-07 08:54:03 +0000

    change article category to em from link

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ const app = function () {
 				<h2>${post.title}</h2>
 				<div class="article-details">
 					<div>By ${post.author} on ${_formatDate(post.timestamp)}</div>
-					<div>Posted in <a href="#">${post.category}</a></div>
+					<div>Posted in <em>${post.category}</em></div>
 				</div>
 				${_formatContent(post.content)}
 			`;
